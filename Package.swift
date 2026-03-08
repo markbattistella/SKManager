38f7123227be1ce07aa2ea44bf5d3d8c748b6eec
Mark Battistella <markbattistella@users.noreply.github.com>
2026-03-08 22:30:53 +1100

    2026-03-08

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/markbattistella/SimpleLogger", from: "25.12.0")
+        .package(url: "https://github.com/markbattistella/SimpleLogger", from: "26.0.0")
     ],
     targets: [
         .target(
